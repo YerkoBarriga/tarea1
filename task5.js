7b6b36ffc72b9db2f9b074628f07f48c4e0a6969
Yerko Barriga <yerkobarriga14@gmail.com>
2024-03-04 22:15:59 -0400

    semaforo_dos_semaforos

--- task5.js
+++ task5.js
@@ -1,19 +1,19 @@
 function ejer5_1() {
     var semaforo = document.getElementById('semaforo');
-
+    let semaforo2 = document.getElementById('semaforo2');
     // Obtener todas las luces del semáforo
     var luces = semaforo.querySelectorAll('.luz');
+    let luces2 = semaforo2.querySelectorAll('.luz2');
     
     // Reiniciar todos los colores a gris
     luces.forEach(luz => luz.style.background = '#ccc');
-
+    luces2.forEach(luz2 => luz2.style.background='#ccc');
     // Establecer los tiempos de cambio
     var tiempoRojo = 3000; // 3 segundos
-    var tiempoAmarillo = 1000; // 1 segundo
-    var tiempoVerde = 2000; // 2 segundos
+    var tiempoAmarillo = 2000; // 2 segundo
+    var tiempoVerde = 3000; // 2 segundos
 
     function cambiarColor(index, color, tiempo) {
-      
       // Encender la luz deseada
       luces[index].style.background = color;
 
@@ -22,6 +22,16 @@ function ejer5_1() {
         luces[index].style.background = '#ccc';
       }, tiempo);
     }
+    function cambiarColor2(index, color, tiempo) {
+        // Encender la luz deseada
+        luces2[index].style.background = color;
+  
+        // Después de un cierto tiempo, apagar la luz
+        setTimeout(function() {
+          luces2[index].style.background = '#ccc';
+          
+        }, tiempo);
+      }
 
     function ejecutarSecuencia() {
       // Iniciar la secuencia
@@ -45,9 +55,31 @@ function ejer5_1() {
         ejecutarSecuencia();
       }, tiempoRojo + tiempoAmarillo + tiempoVerde);
     }
-
+    function ejecutarSecuencia2() {
+        setTimeout(function(){
+            cambiarColor2(2,'#00ff00',tiempoAmarillo+tiempoRojo);
+        },0);
+        setTimeout(function(){
+            cambiarColor2(2,'#ccc',0);
+            cambiarColor2(0,'#ff0000',tiempoRojo);
+        },tiempoRojo+tiempoAmarillo);
+        
+        setTimeout(function(){
+            cambiarColor2(0,'#ccc',0);
+            cambiarColor2(1,'#fff000',tiempoAmarillo);
+            
+        },tiempoRojo+tiempoVerde);
+        
+        setTimeout(function() {
+            cambiarColor(1, '#ccc', 0);
+            // Repetir la secuencia
+            ejecutarSecuencia2();
+          }, tiempoRojo + tiempoAmarillo + tiempoVerde);
+        
+    }
     // Iniciar la secuencia
     ejecutarSecuencia();
+    ejecutarSecuencia2();
 }
   
   
